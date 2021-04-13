fa5c137213eeec62b3cc14ba1e2f4b96b680fa40
Alex Francys <francys_alex2007@hotmail.com>
2021-04-13 16:58:07 -0300

    Versão 1 Cadastro Series

---
+++ Classes/ClasseBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO_Localiza_Cadastro_Series.Classes
+{
+    class ClasseBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Enums/Genero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO_Localiza_Cadastro_Series.Enums
+{
+    enum Genero
+    {
+		Acao,
+		Aventura,
+		Comedia,
+		Documentario,
+		Drama,
+		Espionagem,
+		Faroeste,
+		Fantasia,
+		Ficcao_Cientifica,
+		Musical,
+		Romance,
+		Suspense,
+		Terror
+    }
+}
